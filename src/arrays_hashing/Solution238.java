948217e061d372297e88a9f8474d7e924a73ba96
zhuyidan <454682403@qq.com>
2023-10-21 19:13:25 -0700

    solution 238

--- src/arrays_hashing/Solution238.java
+++ src/arrays_hashing/Solution238.java
@@ -11,14 +11,17 @@ import java.util.Arrays;
 
 public class Solution238 {
   public int[] productExceptSelf(int[] nums) {
-    int[] res = new int[nums.length];
+    //       1 2 3 4
+    //prefix     1 1 2 2*3
+    //postfix   2*3*4 3*4 4 1
     int prefix = 1;
+    int postfix = 1;
+    int[] res = new int[nums.length];
     Arrays.fill(res, 1);
     for (int i = 0; i < nums.length; i++) {
       res[i] = prefix;
       prefix *= nums[i];
     }
-    int postfix = 1;
     for (int i = nums.length - 1; i >= 0; i--) {
       res[i] *= postfix;
       postfix *= nums[i];
