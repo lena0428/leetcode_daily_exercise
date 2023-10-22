35b03682619693d32cc97a49ec23c45855b6ae20
zhuyidan <454682403@qq.com>
2023-10-21 17:09:03 -0700

    Solution 242

---
+++ src/Solution242.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: leetcode
+ * @description: https://leetcode.com/problems/valid-anagram/description/
+ * @author: Yidan
+ * @create: 2023-10-21 17:02
+ **/
+
+public class Solution242 {
+  public boolean isAnagram(String s, String t) {
+    HashMap<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (map.containsKey(s.charAt(i))) {
+        int cnt = map.get(s.charAt(i));
+        cnt++;
+        map.put(s.charAt(i), cnt);
+      } else {
+        map.put(s.charAt(i), 1);
+      }
+    }
+    for (int i = 0; i < t.length(); i++) {
+      if (map.containsKey(t.charAt(i))) {
+        int cnt = map.get(t.charAt(i));
+        cnt--;
+        map.put(t.charAt(i), cnt);
+      } else {
+        return false;
+      }
+    }
+    for (Map.Entry<Character, Integer> entry: map.entrySet()) {
+      if (entry.getValue() != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
